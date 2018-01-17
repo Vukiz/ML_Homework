d80bc1cf7293befca435c741a039b4dc0e8bcf57
Eugene Alekseev <Vukiz1236@gmail.com>
2018-01-17 23:11:36 +0300

    fixed wrong test loading

--- news.py
+++ news.py
@@ -25,18 +25,16 @@ text_clf = text_clf.fit(news_train_data, news_train_data_target)
 
 print("Loading test data...")
 
-news_test = list(csv.reader(open('news_train.txt', 'rt', encoding="utf8"), delimiter='\t'))
+news_test = list(csv.reader(open('news_test.txt', 'rt', encoding="utf8"), delimiter='\t'))
 news_test_data = []
 news_test_data_target = []
 for x in news_test:
-   news_test_data.append(x[2])
-   news_test_data_target.append(x[0])
+    news_test_data.append(x[1])
    
 print("testing")
 
 docs_test = news_test_data
 predicted = text_clf.predict(docs_test)
-print (np.mean(predicted == news_test_data_target))
 print("outputing to final_output.txt")
 fh = open("final_output.txt", 'w')
 for item in predicted:
